7ee07df1eb14c25f96296c0abc6d352c5c00e6fe
imbanghee <imbanghee@gmail.com>
2018-03-29 11:56:39 +0900

    Webserver V1.1

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -11,7 +11,7 @@ public class WebServer {
 	private static final int DEFAULT_PORT = 8080;
 	
     public static void main(String argv[]) throws Exception {
-        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다.
+        // 서버소켓을 생성한다. 웹서버는 기본적으로 8080번 포트를 사용한다. update
     	
     	try (ServerSocket listenSocket = new ServerSocket(DEFAULT_PORT)) {
     		log.info("Web Application Server started {} port.", DEFAULT_PORT);
